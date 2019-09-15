1baf7ff269a61b5fbf3fc863e211a15ffe9e6b08
FSDonnelly <donnelly_frank@yahoo.com>
2019-09-14 21:32:51 -0700

    using webdev project as a boilerplate, started refactor of Navbar and some CSS

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './images';
-
-function App() {
-    return (
-    <div>Hello there</div>
-    
-    );
-  }
+import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
